6ed883d28d6807b6213f6393cc5923c1e97e6ee1
Herbert Fagundes <herbertfag.dev@gmail.com>
2025-02-25 11:30:20 -0300

    feat: add initial restaurant page and menu page

---
+++ src/components/consumption-method-option.tsx
@@ -0,0 +1,44 @@
+import type { ConsumptionMethod } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "./ui/button";
+import { Card, CardContent } from "./ui/card";
+
+interface ConsumptionMethodOptionProps {
+  slug: string;
+  imageUrl: string;
+  imageAlt: string;
+  buttonText: string;
+  option: ConsumptionMethod;
+}
+
+const ConsumptionMethodOption = ({
+  slug,
+  imageUrl,
+  imageAlt,
+  buttonText,
+  option,
+}: ConsumptionMethodOptionProps) => {
+  return (
+    <Card>
+      <CardContent className="flex flex-col items-center gap-8 py-8">
+        <div className="relative h-[80px] w-[80px]">
+          <Image
+            src={imageUrl}
+            fill
+            alt={imageAlt}
+            className="object-contain"
+          ></Image>
+        </div>
+        <Button variant="secondary" className="rounded-full" asChild>
+          <Link href={`/${slug}/menu?consumptionMethod=${option}`}>
+            {buttonText}
+          </Link>
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ConsumptionMethodOption;
