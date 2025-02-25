060cb5e3ddd2113b45aed073fbfdf73e0b1755d8
Herbert Fagundes <herbertfag.dev@gmail.com>
2025-02-25 15:06:44 -0300

    feat: add products details page

---
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import type { Prisma } from "@prisma/client";
+import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/helpers/format-currency";
+
+interface ProductDetailsProps { 
+    product: Prisma.ProductGetPayload<{
+        include: {
+            restaurant: {
+                select: {
+                    avatarImageUrl: true,
+                    name: true,
+                };
+            };
+        };
+    }>;
+}
+
+const ProductDetails = ({ product }: ProductDetailsProps) => {
+    const [quantity, setQuantity] = useState<number>(1);
+    const handleDecreaseQuantity = () => {
+        setQuantity((prev) => {
+            if (prev === 1) return prev;
+            return prev - 1;
+        })
+     }
+    const handleIncreaseQuantity = () => setQuantity((prev) => prev + 1);
+
+    return ( 
+        <div className="relative z-50 mt-[-1.5rem] flex-auto rounded-t-3xl p-5 flex flex-col">
+            <div className="flex-auto">
+                 {/* RESTAURANTE */}
+            <div className="flex items-center gap-1.5 ">
+                <Image
+                    src={product.restaurant.avatarImageUrl}
+                    alt={product.restaurant.name}
+                    width={16}
+                    height={16}
+                    className="rounded-full"
+                />
+                <p className="text-xs text-muted-foreground">
+                    {product.restaurant.name}
+                </p>         
+            </div>
+
+            {/* NOME DO PRODUTO */}
+            <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
+
+            {/* PREÇO E QUANTIDADE */}
+            <div className="flex items-center justify-between">
+                <h3 className="text-lg font-semibold">
+                    {formatCurrency(product.price)}
+                </h3>
+                <div className="flex items-center gap-3 text-center">
+                    <Button variant="outline" className="h-8 w-8 rounded-xl" onClick={handleDecreaseQuantity}>
+                        <ChevronLeftIcon />
+                    </Button>
+                    <p className="w-4">{quantity}</p>
+                    <Button variant="destructive" className="h-8 w-8 rounded-xl>" onClick={handleIncreaseQuantity}>
+                        <ChevronRightIcon />
+                    </Button>
+                </div>
+            </div>
+
+            {/* DESCRIÇÃO */}
+            <div className="mt-6 space-y-3">
+                <h4 className="font-semibold">Sobre</h4>
+                <p className="text-sm text-muted-foreground">{product.description}</p>
+            </div>
+
+            {/* INGREDIENTES */}
+            <div className="mt-6 space-y-3">
+                <div className="flex items-center gap-1 5">
+                    <ChefHatIcon size={18} />
+                     <h4 className="font-semibold">Ingredientes</h4>
+                </div>
+
+                <p className="text-sm text-muted-foreground">{product.ingredients}</p>
+
+                {/* <ul className="list-disc pl-6 text-sm text-muted-foreground">
+                    {product.ingredients.split(',').map(ingredient => (
+                    <li key={ingredient}>{ingredient.trim()}</li>
+                    ))}
+                </ul>       */}
+            </div>
+           </div>
+
+            <Button className=" mt-6 w-full rounded-full">Adicionar à Sacola</Button>
+        </div>
+     );
+}
+ 
+export default ProductDetails;
